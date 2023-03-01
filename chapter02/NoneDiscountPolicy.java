7872e93ed5f764b6ddb7a5a0e47bd5c83c30ebbf
yeon <peach-pie@naver.com>
2023-03-01 23:05:55 +0900

    할인 정책이 적용되어 있지 않은 영화의 정책 추가
    NoneDiscountPolicy 클래스를 추가함으로써 할인 정책이 없는 경우를 예외 케이스로 취급하지 않으면서 할인 요금을 계산할 책임을 그대로 DiscountPolicy 계층에 유지시킨다.

---
+++ chapter02/NoneDiscountPolicy.java
@@ -0,0 +1,9 @@
+package chapter02;
+
+public class NoneDiscountPolicy extends DiscountPolicy {
+    @Override
+    protected Money getDiscountAmount(Screening Screening) {
+        return Money.ZERO;
+    }
+}
+// NoneDiscountPolicy 클래스를 추가함으로써 할인 정책이 없는 경우를 예외 케이스로 취급하지 않으면서 할인 요금을 계산할 책임을 그대로 DiscountPolicy 계층에 유지시킨다.
